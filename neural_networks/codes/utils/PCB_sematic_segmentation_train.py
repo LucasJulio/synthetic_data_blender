0e9f766884269efd429524dc6c793762dbf36669
Lucas Ribeiro <lucas.julio@mvisia.com.br>
2020-07-20 19:01:37 -0300

    Major improvements to training script; Repository cleanup
    - Increased input size of models
    - Implemented mild data augmentation
    - Better overall hyper-parameters
    - Added some more examples to PCB_v1 dataset annotations
    - Cleaned up some old files from repo

--- neural_networks/codes/utils/PCB_sematic_segmentation_train.py
+++ neural_networks/codes/utils/PCB_sematic_segmentation_train.py
@@ -1,10 +1,13 @@
-from PIL import Image
-import tensorflow as tf
-from tensorflow_examples.models.pix2pix import pix2pix
-import matplotlib.pyplot as plt
 import os
 import numpy as np
-from neural_networks.codes.package import PCB_dataset
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow_examples.models.pix2pix import pix2pix
+from neural_networks.codes.utils import PCB_dataset
+from PIL import Image
+
 
 DATASETS_MAIN_PATH = os.path.expanduser("/datasets/")
 SELECTED_DATASET = "sample_dataset"
@@ -35,9 +38,36 @@ def normalize(input_image, input_mask):
 
 
 @tf.function
+def augment_data(input_img, input_mask):
+    """
+    :param input_img: input tensor to be augmented
+    :param input_mask: just the same mask
+    :return: tensor with the following possible augmentations: brightness changes, contrast changes,
+    HUE channel changes
+    """
+    # Brightness
+    if tf.random.uniform(()) > 0.2:
+        input_img = tf.image.random_brightness(input_img, 0.4)
+
+    # Contrast
+    if tf.random.uniform(()) > 0.2:
+        input_img = tf.image.random_contrast(input_img, 0.8, 1.2)
+
+    # HUE mess
+    if tf.random.uniform(()) > 0.2:
+        input_img = tf.image.random_hue(input_img, 0.05)
+
+    augmented_img = input_img
+
+    return augmented_img, input_mask
+
+
+@tf.function
 def load_image_train(datapoint):
-    input_image = tf.image.resize(datapoint['image'], (128, 128))
-    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize_with_crop_or_pad(datapoint['image'], 540, 540)  # eliminates unnecessary borders
+    input_mask = tf.image.resize_with_crop_or_pad(datapoint['segmentation_mask'], 540, 540)
+    input_image = tf.image.resize(input_image, (448, 448))
+    input_mask = tf.image.resize(input_mask, (448, 448))
 
     if tf.random.uniform(()) > 0.5:
         input_image = tf.image.flip_left_right(input_image)
@@ -49,24 +79,28 @@ def load_image_train(datapoint):
 
 
 def load_image_test(datapoint):
-    input_image = tf.image.resize(datapoint['image'], (128, 128))
-    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize_with_crop_or_pad(datapoint['image'], 540, 540)  # eliminates unnecessary borders
+    input_mask = tf.image.resize_with_crop_or_pad(datapoint['segmentation_mask'], 540, 540)
+    input_image = tf.image.resize(input_image, (448, 448))
+    input_mask = tf.image.resize(input_mask, (448, 448))
 
     input_image, input_mask = normalize(input_image, input_mask)
 
     return input_image, input_mask
 
 
-BATCH_SIZE = 4
-BUFFER_SIZE = 1000
-STEPS_PER_EPOCH = 10
+TRAIN_LENGTH = 1500
+BATCH_SIZE = 32
+BUFFER_SIZE = 1500
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
 
 train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 test = dataset['test'].map(load_image_test)
 
-train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+# TODO: confirm that augmentation worked
+train_dataset = train.cache().map(augment_data).shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
 train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-test_dataset = test.batch(2)
+test_dataset = test.batch(BATCH_SIZE)
 
 
 def display(display_list):
@@ -84,19 +118,20 @@ def display(display_list):
 
 for image, mask in train.take(1):
     sample_image, sample_mask = image, mask
-display([sample_image, sample_mask])
+    display([sample_image, sample_mask])
 
 OUTPUT_CHANNELS = 4
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+# TODO: try to change this
+base_model = tf.keras.applications.MobileNetV2(input_shape=[448, 448, 3], include_top=False)
 
 # Use the activations of these layers
 layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
+    'block_1_expand_relu',
+    'block_3_expand_relu',
+    'block_6_expand_relu',
+    'block_13_expand_relu',
+    'block_16_project',
 ]
 layers = [base_model.get_layer(name).output for name in layer_names]
 
@@ -114,7 +149,7 @@ up_stack = [
 
 
 def unet_model(output_channels):
-    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+    inputs = tf.keras.layers.Input(shape=[448, 448, 3])
     x_i = inputs
 
     # Downsampling through the model
@@ -135,24 +170,22 @@ def unet_model(output_channels):
 
 
 model = unet_model(OUTPUT_CHANNELS)
-model.compile(optimizer='adam',
+model.compile(optimizer=Adam(learning_rate=0.001, amsgrad=False),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-tf.keras.utils.plot_model(model, show_shapes=True)
 
-
-def create_mask(pred_mask):
-    pred_mask = tf.argmax(pred_mask, axis=-1)
-    pred_mask = pred_mask[..., tf.newaxis]
-    return pred_mask[0]
+def create_mask(p_mask):
+    p_mask = tf.argmax(p_mask, axis=-1)
+    p_mask = p_mask[..., tf.newaxis]
+    return p_mask[0]
 
 
 def show_predictions(ds=None, num=1):
     if ds:
-        for _, img, msk in dataset["test"].take(num):
-            pred_mask = model.predict(img)
-            display([img[0], msk[0], create_mask(pred_mask)])
+        for im, msk in ds.take(num):
+            pred_mask = model.predict(im[tf.newaxis, ...])
+            display([im[0], msk[0], create_mask(pred_mask)])
     else:
         display([sample_image, sample_mask, create_mask(model.predict(sample_image[tf.newaxis, ...]))])
 
@@ -166,15 +199,19 @@ class DisplayCallback(tf.keras.callbacks.Callback):
         print('\nSample Prediction after epoch {}\n'.format(epoch+1))
 
 
-EPOCHS = 50
-VAL_SUBSPLITS = 1
-VALIDATION_STEPS = 2
+checkpoint = ModelCheckpoint(filepath="/home/ribeiro-desktop/blender_experiments/neural_networks/logs/current_run",
+                             save_best_only=True)
+
+VALIDATION_LENGTH = 500
+EPOCHS = 40
+VAL_SUBSPLITS = 10
+VALIDATION_STEPS = VALIDATION_LENGTH//BATCH_SIZE//VAL_SUBSPLITS
 
 model_history = model.fit(train_dataset, epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
                           validation_steps=VALIDATION_STEPS,
                           validation_data=test_dataset,
-                          callbacks=[DisplayCallback()])
+                          callbacks=[DisplayCallback(), checkpoint])
 
 loss = model_history.history['loss']
 val_loss = model_history.history['val_loss']
@@ -191,27 +228,40 @@ plt.ylim([0, 1])
 plt.legend()
 plt.show()
 
+# Load best model from current run
+best = tf.keras.models.load_model("/home/ribeiro-desktop/blender_experiments/neural_networks/logs/current_run")
 
-# TODO: fix this
+for img, mask in test.take(1):
+    test_image, test_mask = img, mask
+    predicted_mask = create_mask(best.predict(test_image[tf.newaxis, ...]))
+    display([test_image, test_mask, predicted_mask])
+
+'''
+a = test.as_numpy_iterator().next()
+plt.imshow(a[0])
+plt.show()
+'''
+
+print("Train finished. Making some inferences")
+
+
 def normalize_input(input_image):
     input_image = tf.cast(input_image, tf.float32) / 255.0
     return input_image
 
 
-def inference_on_image(image_file_path, classifier_model, show=True):
+def inference_on_image(image_file_path, classifier_model):
     target_size = max(classifier_model.layers[0].input_shape)
-    img = Image.open(image_file_path)
-    img = img.resize((target_size[1], target_size[2]), Image.ANTIALIAS)
-    np_img = np.asarray(img)
+    im = Image.open(image_file_path)
+    im = im.resize((target_size[1], target_size[2]), Image.ANTIALIAS)
+    np_img = np.asarray(im)
 
     img_batch = np.expand_dims(np_img, axis=0)
     pre_processed_input = normalize_input(img_batch)[:, :, :, :3]
-    pred_mask = classifier_model.predict(pre_processed_input)
-    if show:
-        plt.imshow(np_img)
-        plt.show()
-        plt.imshow(pred_mask[0, :, :, :])
-        plt.show()
+    pred_mask = create_mask(classifier_model.predict(pre_processed_input))
+    display([np_img, pred_mask])
 
 
-print("train finished")
+for i in range(1, 31):
+    inference_on_image("/home/ribeiro-desktop/Pictures/Webcam/ri_" + str(i).zfill(7) + ".jpg", best)
+    inference_on_image("/home/ribeiro-desktop/Pictures/Webcam/ri_" + str(i).zfill(7) + ".jpg", model)
