ac5efa5519e6b950e67fe3ba6af7ec6dd1cd7409
PC-VITOR\Vítor <vitorhi@msn.com>
2020-10-10 18:50:08 -0300

    implementação de método para iluminação usando imagens HDRI e adição de textura ao plano

--- python_scripts/utils/set_ids.py
+++ python_scripts/utils/set_ids.py
@@ -1,4 +1,11 @@
 import bpy
+from bpy import ops
+
+blend_file = "arduino_uno.blend"
+blend_folder = "A:/Documentos/GitHub/synthetic_data_blender/Kicad files/Arduino_Uno/"
+render_folder = "A:/Documentos/GitHub/synthetic_data_blender/render_arduino"
+
+ops.wm.open_mainfile(filepath=blend_folder + blend_file)
 
 scene = bpy.context.scene
 
@@ -15,12 +22,14 @@ for obj in foo_objs:
     obj.pass_index=id
 
 # Set only the ids of the objects in Arduino collection and not in Texto
-# TODO: Text must be with the same index of PCB? 
+# TODO: Text must be with the same index of PCB?
 id=1
 for obj in bpy.data.collections['Arduino'].all_objects:
     if obj.users_collection[0].name != 'Texto':
         obj.pass_index=id
         id+=1
+    # else:
+    #     obj.pass_index=
 
 # Set same id to children
 for obj in bpy.data.collections['Arduino'].all_objects:
